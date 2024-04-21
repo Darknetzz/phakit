7b4a6571b4bb2fa035f47b19e844035a8758e245
Kristian Røste <github@roste.org>
2024-04-21 15:30:16 +0000

    chore: Update site title and base URL in config.php

--- deploy/index.php
+++ deploy/index.php
@@ -1,14 +1,14 @@
+<?php
+    require_once('config.php');
+?>
+
 <!DOCTYPE html>
-<html>
+<html data-bs-theme="<?= THEME ?>">
 
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-        <?php
-            require_once('config.php');
-        ?>
 
         <title><?= SITE_TITLE ?></title>
 
