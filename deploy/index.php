838f8ec5ebfb8b1f0c2ef7c67180166d8f691848
Kristian Røste <github@roste.org>
2024-04-21 16:05:15 +0000

    make phakit class

--- deploy/index.php
+++ deploy/index.php
@@ -1,28 +1,17 @@
 <?php
-    require_once('config.php');
+    require_once('phakit/phakit.php');
+    define("PHAKIT", new Phakit());
 ?>
 
 <!DOCTYPE html>
-<html data-bs-theme="<?= THEME ?>">
+<html data-bs-theme="<?= PHAKIT_CONFIG['THEME'] ?>">
 
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <title><?= SITE_TITLE ?></title>
-
-        <?php
-            if (defined('FRONTEND')) {
-                    echo '<script src="js/jquery.min.js"></script>';
-                    echo '<script src="js/'.FRONTEND.'.min.js"></script>';
-                    echo '<link href="css/'.FRONTEND.'.min.css" rel="stylesheet" />';
-            }
-
-            if (defined('ICONS')) {
-                echo '<link href="css/'.ICONS.'-icons.min.css" rel="stylesheet" />';
-            }
-        ?>
+        <title><?= PHAKIT_CONFIG['SITE_TITLE'] ?></title>
 
     </head>
 
---
+++ deploy/phakit/phakit.php
@@ -0,0 +1,50 @@
+<?php
+
+class Phakit {
+
+    /* ────────────────────────────────────────────────────────────────────────── */
+    /*                                 __construct                                */
+    /* ────────────────────────────────────────────────────────────────────────── */
+    function __construct() {
+
+        require_once('config.php');
+        $this->config = PHAKIT_CONFIG;
+
+        if (!empty($this->config['FRONTEND'])) {
+            echo '<script src="js/jquery.min.js"></script>';
+            echo '<script src="js/'.$this->config['FRONTEND'].'.min.js"></script>';
+            echo '<link href="css/'.$this->config['FRONTEND'].'.min.css" rel="stylesheet" />';
+        }
+
+        if (!empty($this->config['ICONS'])) {
+            echo '<link href="css/'.$this->config['ICONS'].'-icons.min.css" rel="stylesheet" />';
+        }
+    }
+
+    /* ────────────────────────────────────────────────────────────────────────── */
+    /*                                    icon                                    */
+    /* ────────────────────────────────────────────────────────────────────────── */
+    function icon($icon = '') {
+        if (!empty($this->config['ICONS'])) {
+            if ($this->config['ICONS'] == 'tabler') {
+                return '<i class="ti ti-'.$icon.'"></i>';
+            } else if ($this->config['ICONS'] == 'bootstrap') {
+                return '<i class="bi bi-'.$icon.'"></i>';
+            }
+        }
+    }
+
+    /* ────────────────────────────────────────────────────────────────────────── */
+    /*                                    alert                                   */
+    /* ────────────────────────────────────────────────────────────────────────── */
+    function alert($type = 'info', $message = '') {
+        return '<div class="alert alert-'.$type.' alert-dismissible fade show" role="alert">
+                    '.$message.'
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+    }
+
+}
+
+
+?>
