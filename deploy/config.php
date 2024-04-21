1c807fb2bb53ed6aff2df1548517451f998515d4
Kristian Røste <github@roste.org>
2024-04-21 15:28:16 +0000

    chore: Update site title and base URL in config.php

--- deploy/config.php
+++ deploy/config.php
@@ -4,13 +4,22 @@
 /*                                 CONFIG.PHP                                 */
 /* ────────────────────────────────────────────────────────────────────────── */
 # Specify your project's settings here
-# Define the site title and base URL
+
+/* ─────────────────────────────── SITE_TITLE ─────────────────────────────── */
+# Define the site title
 define("SITE_TITLE", "My Website");
+
+/* ─────────────────────────────── BASE_URL ──────────────────────────────── */
+# Define the base URL
 define("BASE_URL", "https://".$_SERVER['HTTP_HOST']);
 
-# Define the frontend
+/* ──────────────────────────────── FRONTEND ──────────────────────────────── */
 # Options: "tabler", "bootstrap"
 # Leave empty to disable
 define("FRONTEND", "tabler");
+
+/* ────────────────────────────────── THEME ───────────────────────────────── */
+# Options: "light", "dark"
+define("THEME", "dark");
 
 ?>
